4d73d4b0a3c0a56c7ce031ae2fd4816dc409b9e5
asow1802 <sowablaye1802@gmail.com>
2024-05-08 19:23:15 +0000

    05-82024

--- cours-tan/tan/eqnonlin/fixpoint.py
+++ cours-tan/tan/eqnonlin/fixpoint.py
@@ -1,8 +1,20 @@
-def fixpoint(phi,s, x0, tol, nmax, *args):
+def fixpoint(phi, x0, tol, nmax, *args):
+    """
+    FIXPOINT Fixed point iterations.
+    P=FIXPOINT(PHI,X0,TOL,NMAX) tries to find the fixed point P of the 
+    continuous and differentiable function PHI using fixed point iterations 
+    starting from X0. PHI is a function which accepts real scalar input x 
+    and returns a real scalar value. If the search fails, an error message is displayed.
+    
+    [P,RES,NITER]= FIXPOINT(PHI,...) returns the value of the residual in ZERO
+    and the iteration number at which ZERO was computed.
+    
+    [P,RES,NITER,INC]= FIXPOINT(PHI,...) returns a list INC with the absolute values of the
+    differences between successive approximations (increments).
+    """
     x = x0
-    phix = phi(x,s, *args)
+    phix = phi(x, *args)
     niter = 0
-    sol = []
     diff = tol + 1
     inc = [x0]
     
@@ -10,10 +22,14 @@ def fixpoint(phi,s, x0, tol, nmax, *args):
         niter += 1
         diff = abs(phix - x)
         x = phix
-        phix = phi(x,s, *args)
-        inc.append(diff) 
+        phix = phi(x, *args)
+        inc.append(diff)
+        
+    if niter >= nmax:
+        print('fixpoint stopped without converging to the desired tolerance',
+              'because the maximum number of iterations was reached')
+        
     p = x
     res = phix - x
     
-    return p, niter, inc 
-
+    return p, res, niter, inc
